bb67cf54e1cd4a3e8a2d970e73a583c3eb02c149
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2025-09-10 18:08:49 -0600

    Enrutamiento rutas

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const usuariosRouter = require('./routes/usuarios');
+const citasRouter = require('./routes/citas');
+const serviciosRouter = require('./routes/servicios');
 
 const app = express();
 app.use(cors({
@@ -12,6 +14,9 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/usuarios', usuariosRouter);
+app.use('/citas', citasRouter);
+app.use('/servicios', serviciosRouter);
+
 
 app.get('/', (req, res) => res.send('API funcionando'));
 
