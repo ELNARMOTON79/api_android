aa923608eb045e4a95f6eaafbddb5220de57c191
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2025-09-16 14:39:57 -0600

    Endpoint login corregido4

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 // POST /login
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
   const { correo, contraseña } = req.body;
   try {
     const { rows } = await pool.query(
